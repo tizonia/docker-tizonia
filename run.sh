096ec3b197b44d0246757d6d7b06b3c692d2c049
Matt Armand <marmand68@gmail.com>
2020-03-13 13:07:42 -0400

    Lint existing shell files

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ UNAME=${UNAME:-tizonia}
 # lets also change the UID and GID of the default
 # user in the container
 if [[ ${PUID} != 1000 || ${PGID} != 1000 ]]; then
-    groupmod -o -g "${PGID}" ${UNAME};
-    usermod -o -u "${PUID}" ${UNAME};
-    chown ${UNAME}:${UNAME} /home/${UNAME};
+    groupmod -o -g "${PGID}" "${UNAME}";
+    usermod -o -u "${PUID}" "${UNAME}";
+    chown "${UNAME}":"${UNAME}" "/home/${UNAME}";
 fi
 
 
